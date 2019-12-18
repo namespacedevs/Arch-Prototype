eb6b321d411983700a0b2ce4dd354ae92ae396f3
Uigor Marshall <uigor.silva@acad.ifma.edu.br>
2019-12-18 17:04:10 -0300

    Add attribute email to user model

--- Arch-Prototype.Domain/Auth/User.cs
+++ Arch-Prototype.Domain/Auth/User.cs
@@ -4,6 +4,7 @@ namespace Arch_Prototype.Domain.Auth
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Email { get; set; }
         public string Password { get; set; }
     }
 }
